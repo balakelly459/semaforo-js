f5de6c9677b929b71032e823bea6433082ad49ae
balakelly459 <21192911@CFP127ALUNO.LOCAL>
2021-08-31 10:57:29 -0300

    cores ok

--- semaforo.js
+++ semaforo.js
@@ -1,2 +1,26 @@
 "use strict"
 
+const semaforo = document.getElementById("semaforo")
+let idInterval
+
+function botoesCores(corVermelho, corAmarelo, corVerde){
+    const vermelho = document.getElementById("vermelho")
+    const amarelo = document.getElementById("amarelo")
+    const verde = document.getElementById("verde")
+}
+
+function semaforoVermelho() {
+    document.getElementById("semaforo").src = "img/vermelho.png"
+}
+function semaforoAmarelo() {
+    document.getElementById("semaforo").src = "img/amarelo.png"
+}
+function semaforoVerde() {
+    document.getElementById("semaforo").src = "img/verde.png"
+}
+
+
+//eventos
+document.getElementById("vermelho").addEventListener("click", semaforoVermelho)
+document.getElementById("amarelo").addEventListener("click", semaforoAmarelo)
+document.getElementById("verde").addEventListener("click", semaforoVerde)
